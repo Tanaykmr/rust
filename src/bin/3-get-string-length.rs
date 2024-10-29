da190bad0685445968b5341daf82c931372de62b
Tanay Kumar <tanaykmrdev@gmail.com>
2024-10-29 08:53:05 +0530

    3-get-string-length

---
+++ src/bin/3-get-string-length.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let name = String::from("helloworld");
+    let len = get_str_len(name);
+    println!("The length of the string is {}", len);
+}
+
+
+fn get_str_len(str: String) -> usize{
+    str.chars().count() 
+}
