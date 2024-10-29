4d84052d613908f095d8eedca0b1df3a85ac47b6
Tanay Kumar <tanaykmrdev@gmail.com>
2024-10-29 08:39:23 +0530

    is_even and fib

---
+++ src/bin/1-is_even.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("{}", is_even(20)); // this is not a function it is a macro. th "{}" tells the compiler to print something dynamic here, and the 2nd argument is what will be printed
+}
+
+// i32 is signed, u32 is un
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    }
+    return false;
+}
+
---
+++ src/bin/2-fib.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let x: i32 = 5;
+    println!("{}", fib(x));
+}
+
+// 0 1 1 2 3 5 8 13
+
+fn fib(num: i32) -> i32 {
+    let mut first = 0; // mutable vars are vars whose value you can change. Think of them as non-constants. Every variable you define in rust, by default, is a constant
+    let mut second = 1;
+    if num == 0 {
+        return first;
+    }
+    if num == 1 {
+        return second;
+    }
+
+    for _ in r..num - 1 { // we used _ since we're not using the value of i in the for loop
+        let temp = second;
+        second = second + first;
+        first = temp;
+    }
+    return second;
+}
